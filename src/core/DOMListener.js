b68928b887e034e044f910479f26e3236d8fe31b
Nikita Mirolyubov <mirolyubov_n@yahoo.co.uk>
2021-06-27 13:27:57 +0500

    added basic  functionality of framework

---
+++ src/core/DOMListener.js
@@ -0,0 +1,40 @@
+import capitalize from './utils';
+
+function getMethodName(eventName) {
+	return 'on' + capitalize(eventName);
+}
+
+export default class DOMListener {
+	constructor($root, listeners = []) {
+		if (!$root) {
+			throw new Error('No $root provided for DOMListenter');
+		}
+		this.$root = $root;
+		this.listeners = listeners;
+	}
+
+	initDOMListeners() {
+		this.listeners.forEach(listener => {
+			const method = getMethodName(listener);
+
+			if (!this[method]) {
+				throw new Error(`Method ${method} is not implemented in ${this.name} component`);
+			}
+
+			this[method] = this[method].bind(this);
+			this.$root.on(listener, this[method]);
+		});
+	}
+
+	removeDOMListeners() {
+		this.listeners.forEach(listener => {
+			const method = getMethodName(listener);
+
+			if (!this[method]) {
+				throw new Error(`Method ${method} is not implemented in ${this.name} component`);
+			}
+
+			this.$root.off(listener, this[method]);
+		});
+	}
+}
